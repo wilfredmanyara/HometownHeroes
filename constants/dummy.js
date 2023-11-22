c1372a4920b015b36bcd9d73d8a0a27c3a00febe
Wilfred Manyara <manyarawilly46@gmail.com>
2023-11-22 12:10:49 +0300

    final one

--- constants/dummy.js
+++ constants/dummy.js
@@ -7,57 +7,57 @@ const NFTData = [
     creator: "Putri Intan",
     price: 0.99,
     description:
-      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural.",
+      "Welcome to Brits Coffee Place, your cozy caffeine haven in the heart of town. With its charming British-inspired decor, this local gem offers a warm and inviting atmosphere. Sip on expertly brewed coffee while enjoying the friendly chatter and community spirit. ",
     image: assets.nft01,
     bids: [
       {
         id: "BID-11",
         name: "Jessica Tan",
-        price: 4.25,
+        price: 1.00,
         image: assets.person02,
-        date: "December 12, 2019 at 12:10 PM",
+        date: "November 21, 2023 at 12:10 PM",
       },
       {
         id: "BID-12",
         name: "Jennifer Sia",
-        price: 4.5,
+        price: 0.79,
         image: assets.person03,
-        date: "December 27, 2019 at 1:50 PM",
+        date: "November 27, 2023 at 1:50 PM",
       },
       {
         id: "BID-13",
         name: "Rosie Wong",
-        price: 4.75,
+        price: 2.10,
         image: assets.person04,
-        date: "December 31, 2019 at 3:50 PM",
+        date: "November 31, 2023 at 3:50 PM",
       },
     ],
   },
   {
     id: "NFT-02",
-    name: "Green Coins",
+    name: "Artisanal Bakeshop",
     creator: "Siti Nurhaliza",
-    price: 7.25,
+    price: 2.49,
     description:
-      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis.",
+      "A local bakery known for its artisanal bread, pastries, and cakes made from high-quality ingredients, creating a delightful experience for customers.",
     image: assets.nft02,
     bids: [
       {
         id: "BID-21",
         name: "Jessica Tan",
-        price: 7.05,
+        price: 2.10,
         image: assets.person04,
-        date: "December 12, 2019 at 12:10 PM",
+        date: "November 12, 2023 at 12:10 PM",
       },
     ],
   },
   {
     id: "NFT-03",
-    name: "NFT coins race",
+    name: "Adventure Gear Outfitters",
     creator: "Elisabeth aho",
-    price: 95.25,
+    price: 90.99,
     description:
-      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis. Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis.",
+      "A store catering to outdoor enthusiasts, offering a diverse range of gear and equipment for activities such as hiking, camping, and backpacking.",
     image: assets.nft03,
     bids: [
       {
@@ -65,24 +65,24 @@ const NFTData = [
         name: "Jessica Tan",
         price: 95.25,
         image: assets.person02,
-        date: "December 12, 2019 at 12:10 PM",
+        date: "November 12, 2019 at 12:10 PM",
       },
       {
         id: "BID-32",
         name: "Jennifer Sia",
         price: 95.5,
         image: assets.person03,
-        date: "December 27, 2019 at 1:50 PM",
+        date: "November 27, 2019 at 1:50 PM",
       },
     ],
   },
   {
     id: "NFT-04",
-    name: "Nifty NFT",
+    name: "nft04nft",
     creator: "Putri Intan",
     price: 54.25,
     description:
-      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural.Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur.",
+      "A store specializing in art and craft supplies, offering a wide range of materials for hobbyists, artists, and DIY enthusiasts to unleash their creativity.",
     image: assets.nft04,
     bids: [
       {
@@ -90,7 +90,7 @@ const NFTData = [
         name: "Jessica Tan",
         price: 56.25,
         image: assets.person02,
-        date: "December 12, 2019 at 12:10 PM",
+        date: "November 12, 2019 at 12:10 PM",
       },
       {
         id: "BID-42",
@@ -117,11 +117,11 @@ const NFTData = [
   },
   {
     id: "NFT-05",
-    name: "Colorful circles",
+    name: "Sew Elegant Boutique",
     creator: "David doe",
     price: 10.25,
     description:
-      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural.",
+      "A boutique offering expert clothing alterations and tailoring services, catering to individuals who seek custom-fitted and stylish attire.",
     image: assets.nft05,
     bids: [
       {
@@ -135,11 +135,11 @@ const NFTData = [
   },
   {
     id: "NFT-06",
-    name: "Black box model",
+    name: "FitZone Fitness Studio",
     creator: "Leo Messi",
     price: 20.25,
     description:
-      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis.",
+      "A community-focused fitness studio offering a range of classes, personal training, and wellness programs to help people achieve their health and fitness goals.",
     image: assets.nft06,
     bids: [
       {
@@ -181,11 +181,11 @@ const NFTData = [
   },
   {
     id: "NFT-07",
-    name: "Abstracto soulful art",
+    name: "Book Haven",
     creator: "Victor de la Cruz",
     price: 18.25,
     description:
-      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis. Ullamcorper platea penatibus ornare egestas nulla ligula hendrerit nisl suscipit sociosqu maximus, tincidunt aptent habitant purus pharetra ultrices dapibus laoreet nisi lacinia. Porta malesuada netus vel sapien conubia porttitor aliquam ut pretium ante litora molestie senectus magna egestas sociosqu, eget aliquet fames pharetra felis posuere varius fringilla quisque in arcu montes eu ullamcorper.",
+      "A cozy independent bookstore that specializes in a curated selection of books, hosting book clubs, and promoting a love for literature within the community.",
     image: assets.nft07,
     bids: [],
   },
